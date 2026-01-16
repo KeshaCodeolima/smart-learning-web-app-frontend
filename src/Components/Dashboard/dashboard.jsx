756a37d21d1ea7abd55509be6ffd8e94d62386c5
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2026-01-16 12:38:12 +0530

    (dashboard):Add some messages using the span tag to each box.

--- src/Components/Dashboard/dashboard.jsx
+++ src/Components/Dashboard/dashboard.jsx
@@ -14,18 +14,22 @@ function Dashboard() {
                         <div className="dashvideo">
                             <img src={Camera} alt="Camera" className='image' />
                             <p>Watch video with Your Emotions</p>
+                            <span>Adjusts video playback speed based on your emotions understanding.</span>
                         </div>
                         <div className="dashquiz">
                             <img src={Quiz} alt="Quiz" className='image' />
                             <p>Answer Your Quiz</p>
+                            <span>Creates quizzes based on your learning progress to test your knowledge.</span>
                         </div>
                         <div className="dashshort">
                             <img src={ShortNote} alt="ShortNote" className='image' />
                             <p>Make Short Notes</p>
+                            <span>Lets you quickly make notes while studying to summarize important points.</span>
                         </div>
                         <div className="dashprogres">
                             <img src={Progres} alt="Progress" className='image' />
                             <p>Check Your Progress</p>
+                            <span>Shows your learning stats, like quiz scores and weak areas, over time.</span>
                         </div>
                     </div>
                 </div>
