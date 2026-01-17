173e2834346cffd3e4c3f6929f477750be6e7c51
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2026-01-17 09:53:18 +0530

    (dashboard): Add the new dashboardvideo page to App.js file to do the navigation part.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./Components/Header/header";
 import Signup from "./Components/Signup/signup";
 import Login from "./Components/Login/login";
 import Dashboard from "./Components/Dashboard/dashboard";
+import Dashboardviode from "./Components/Dashboard/dashboardviode";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Route path ="/login" element={<Login/>}/>
       <Route path ="/signup" element={<Signup/>}/>
       <Route path = "/" element={<Dashboard/>}/>
+      <Route path="/video" element={<Dashboardviode/>}/>
     </Routes>
     </BrowserRouter>
     </>
