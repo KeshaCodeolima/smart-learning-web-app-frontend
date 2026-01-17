825efca05612095f5cf4d6d12485d838d01785aa
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2026-01-17 11:05:24 +0530

    (dashboard):create the new file call dashboardquiz.jsx inside the dashboradquiz folder to add the quiz adding page coding part.

---
+++ src/Components/Dashboard/dashboradquiz/dashboardquiz.jsx
@@ -0,0 +1,33 @@
+import './dashboardquiz.css';
+import { Link } from 'react-router-dom'
+
+function Dashboardquiz() {
+    return (
+        <>
+            <div className="quizmain">
+                <h2>Create Quiz</h2>
+                <div className="quizmain2">
+                    <div className="quiznormal">
+                        <span>Create New Quiz</span>
+                        <input type="text" name="" id="" placeholder='Quiz Title' />
+                        <textarea
+                            name="question"
+                            id="question"
+                            placeholder='Add Question / Prompt here...'
+                            className='inputbox'
+                            rows="4"
+                        />
+                        <button>Create Quiz</button>
+                    </div>
+                    <div className="quizauto">
+                        <span>Automated Quiz from Video</span>
+                        <button>Create Quiz</button>
+                    </div>
+                </div>
+                <Link to={'/'}><button>Back to Dashboard</button></Link>
+            </div>
+        </>
+    )
+}
+
+export default Dashboardquiz
