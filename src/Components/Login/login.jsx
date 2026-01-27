c71fdc2aee16d0d4dd5e5b6a41f2f79c988e2bd8
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2026-01-27 12:21:04 +0530

    (logout): Add the localStorage set iteam to login.jsx file to add the user details and to check of the user is login from the loacl storage.

--- src/Components/Login/login.jsx
+++ src/Components/Login/login.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './login.css';
 import { useState } from 'react';
 import axios from 'axios';
@@ -8,6 +8,7 @@ function Login() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [errors, setErrors] = useState('');
+    const navigate = useNavigate();
 
     const validation = () => {
         const newErrors = {};
@@ -28,15 +29,18 @@ function Login() {
         const ValidationErrors = validation();
         if (Object.keys(ValidationErrors).length > 0) {
             setErrors(ValidationErrors);
-        }else{
-            axios.post('http://localhost:5000/api/users/login',{username,password})
-            .then((result)=>{
-                if(result.data === 'Successful login'){
-                    alert("Login Successfully!")
-                }else{
-                    alert(result.data);
-                }
-            }).catch((error)=>{console.log(error)})
+        } else {
+            axios.post('http://localhost:5000/api/users/login', { username, password })
+                .then((result) => {
+                    if (result.data.message === 'Successful Login') {
+                        alert("Login Successfully!")
+                        localStorage.setItem("keepLoggedIn", JSON.stringify(true));
+                        localStorage.setItem("currentuser", JSON.stringify(result.data.user));
+                        navigate("/dashboard")
+                    } else {
+                        alert(result.data);
+                    }
+                }).catch((error) => { console.log(error) })
         }
     }
     return (
@@ -46,13 +50,12 @@ function Login() {
                 <form onSubmit={login}>
                     <div className="logininput">
                         <label>User Name: </label>
-                        <input type="text" onChange={(e)=> setUsername(e.target.value)}/>
+                        <input type="text" onChange={(e) => setUsername(e.target.value)} />
                         {errors.username && <p className='errors'>{errors.username}</p>}
 
                         <label>Password: </label>
-                        <input type="password" onChange={(e)=> setPassword(e.target.value)} />
+                        <input type="password" onChange={(e) => setPassword(e.target.value)} />
                         {errors.password && <p className='errors'>{errors.password}</p>}
-
                     </div>
                     <div className="loginfoget">
                         <label>Forget Password</label>
