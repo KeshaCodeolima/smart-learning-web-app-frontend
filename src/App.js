3d0227d051de1ae67b01a2589e9a90d8fb6cf750
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2026-01-27 12:23:42 +0530

    (logout): Add the localStorage get iteam to App.js file to get the user login or not and move to the dashboard page.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Header from "./Components/Header/header";
 import Signup from "./Components/Signup/signup";
 import Login from "./Components/Login/login";
@@ -7,22 +7,29 @@ import Dashboardviode from "./Components/Dashboard/dashboardvideo/dashboardviode
 import Dashboardquiz from "./Components/Dashboard/dashboradquiz/dashboardquiz";
 import Dashboardnotes from "./Components/Dashboard/dashboardnotes/dashboardnotes";
 import Dashboardprogress from "./Components/Dashboard/dashboardprogress/dashboardprogress";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(JSON.parse(localStorage.getItem("keepLoggedIn")));
+
+  useEffect (() => {
+    const authStatus = JSON.parse(localStorage.getItem("keepLoggedIn"));
+    setIsLoggedIn(authStatus);
+  }, [])
   return (
     <>
-    <BrowserRouter>
-    <Header/>
-    <Routes>
-      <Route path ="/" element={<Login/>}/>
-      <Route path ="/signup" element={<Signup/>}/>
-      <Route path = "/dashboard" element={<Dashboard/>}/>
-      <Route path="/video" element={<Dashboardviode/>}/>
-      <Route path="/quiz" element={<Dashboardquiz/>}/>
-      <Route path="/note" element={<Dashboardnotes/>}/>
-      <Route path="/progress" element={<Dashboardprogress/>}/>
-    </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={isLoggedIn ? <Navigate to={'/dashboard'} /> : <Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/video" element={<Dashboardviode />} />
+          <Route path="/quiz" element={<Dashboardquiz />} />
+          <Route path="/note" element={<Dashboardnotes />} />
+          <Route path="/progress" element={<Dashboardprogress />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
