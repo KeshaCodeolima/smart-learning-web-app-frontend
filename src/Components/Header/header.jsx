8db469bbde631de7ba731bb958ab162f4940b3a5
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2026-01-27 13:21:54 +0530

    (header-details): Add the Home link to dashboard and if it move only when the user is logged to the page.

--- src/Components/Header/header.jsx
+++ src/Components/Header/header.jsx
@@ -2,16 +2,44 @@ import './header.css';
 import Setting from '../../Images/settingicon.png';
 import { useState } from 'react';
 import Settings from './Settings/settings';
+import { useNavigate } from 'react-router-dom';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function Header() {
   const [isopen, setIsopen] = useState(false);
+  const navigate = useNavigate();
+
+  const NotifyWarning = () => {
+    toast.warning("Please Login To Page", {
+      position: "top-center",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+    });
+  }
+
+  const handledash = (e) => {
+    e.preventDefault();
+    const isLoggedIn = JSON.parse(localStorage.getItem("keepLoggedIn"));
+
+    if (isLoggedIn) {
+      navigate('/dashboard')
+    } else {
+      NotifyWarning();
+    }
+  }
   return (
     <>
       <div className="headermain">
         <h2>Smart Learning Web Application </h2>
         <div className="headerlabel">
           <ul>
-            <li>Home</li>
+            <li onClick={handledash}>Home</li>
             <li>About</li>
             <li>Contact Us</li>
           </ul>
@@ -26,6 +54,7 @@ function Header() {
           <Settings />
         </div>
       </div>
+      <ToastContainer />
     </>
   )
 }
