eae1241798c19521412f7957e1d64a2f4b63433e
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2026-02-10 11:17:05 +0530

    (emotion-detection): Add the code to get the adding video to video box in the dashboard.

--- src/Components/Dashboard/dashboardvideo/dashboardviode.jsx
+++ src/Components/Dashboard/dashboardvideo/dashboardviode.jsx
@@ -3,11 +3,15 @@ import './dashboardvideo.css';
 import { Link } from 'react-router-dom'
 
 function Dashboardviode() {
-  const [filename, setFilename]= useState('');
+  const [filename, setFilename] = useState('');
+  const [videoSrc, setVideoSrc] = useState(null);
 
-  const handlefile = (e)=>{
-    if(e.target.files.length > 0 ){
-      setFilename(e.target.files[0].name);
+  const handlefile = (e) => {
+    if (e.target.files.length > 0) {
+      const file = e.target.files[0];
+      setFilename(file.name);
+      const url = URL.createObjectURL(file);
+      setVideoSrc(url);
     }
   };
 
@@ -21,7 +25,15 @@ function Dashboardviode() {
             <span>Video Selected: {filename} </span>
           </div>
           <div className="videoinput">
-            <span>Video</span>
+            {videoSrc ? (
+              <video key={videoSrc} width="100%" controls autoPlay>
+                <source src={videoSrc} type="video/mp4" />
+              </video>
+            ) : (
+              <div className="video-placeholder">
+                <span> Please Select a video to play. </span>
+              </div>
+            )}
           </div>
           <div className="videobtn">
             <Link to={'/dashboard'}><button>Back to Dashboard</button></Link>
