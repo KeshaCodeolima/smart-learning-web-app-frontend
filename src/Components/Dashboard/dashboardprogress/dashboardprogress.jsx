b09e97b1e0743fffcd7b8bb4f8ee2d8868f20e8f
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2026-03-15 23:39:16 +0530

    (progress-tracking): Add the new code lines to get the user quiz details to show in the progress page and add some links to new two pages to show the quiz results.

--- src/Components/Dashboard/dashboardprogress/dashboardprogress.jsx
+++ src/Components/Dashboard/dashboardprogress/dashboardprogress.jsx
@@ -1,31 +1,49 @@
+import { useEffect, useState } from 'react';
 import './dashboardprogress.css';
 import { Link } from 'react-router-dom'
+import axios from 'axios';
 
-function dashboardprogress() {
+function Dashboardprogress() {
+  const [progress, setProgress] = useState({
+    averageprogress: 0,
+    allResults: [],
+    weakTopics: []
+  });
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem('currentuser'));
+    axios.get(`http://localhost:5000/api/users/getdetail?userId=${user.id}`)
+      .then(res => {
+        setProgress(res.data)
+      });
+  }, [])
   return (
     <>
       <div className="progressmain">
         <div className="progressmain2">
-            <h2>Your Learning Progress</h2>
-            <div className="progressresults">
-                <div className="overall">
-                    <span>Overall Score</span>
-                    <p>Average Quiz Score</p>
-                </div>
-                <div className="totaltopic">
-                    <span>Total Topics</span>
-                    <p>Under the Last Week</p>
-                </div>
-                <div className="weekconcept">
-                    <span>Week Concepts</span>
-                    <p>Overall Week Concepts</p>
-                </div>
+          <h2>Your Learning Progress</h2>
+          <div className="progressresults">
+            <div className="overall">
+              <span>Overall Score</span>
+              <p>{progress.averageprogress}%</p>
             </div>
-            <Link to={'/dashboard'}><button>Back to Dashboard</button></Link>
+            <Link to='/totaltopic' state={{ results: progress.allResults }} style={{ textDecoration: 'none' }}>
+              <div className="totaltopic">
+                <span>Total Topic Results</span>
+                <p>Click to View ( {progress.allResults.length} )</p>
+              </div>
+            </Link>
+            <Link to='/weakconcept' state={{ weaktopics: progress.weakTopics }} style={{ textDecoration: 'none' }} >
+              <div className="weekconcept">
+                <span>Week Concepts</span>
+                <p>Click to View ( {progress.weakTopics.length} )</p>
+              </div>
+            </Link>
+          </div>
+          <Link to={'/dashboard'}><button>Back to Dashboard</button></Link>
         </div>
       </div>
     </>
   )
 }
 
-export default dashboardprogress
+export default Dashboardprogress
