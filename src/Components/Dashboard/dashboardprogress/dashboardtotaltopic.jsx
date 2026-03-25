656fe56aaa4b6589b9a32a0d40f545f70e5b55d4
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2026-03-25 23:50:06 +0530

    (localization): Add the localization in to the dashboardtotaltopic.jsx file.

--- src/Components/Dashboard/dashboardprogress/dashboardtotaltopic.jsx
+++ src/Components/Dashboard/dashboardprogress/dashboardtotaltopic.jsx
@@ -1,23 +1,25 @@
 import { Link, useLocation } from "react-router-dom";
 import './dashboardprogress.css';
+import { useTranslation } from "react-i18next";
 
 function Dashboardtotaltopic() {
     const location = useLocation();
     const results = location.state?.results || [];
+    const { t } = useTranslation();
 
     return (
         <div className="totaltopic-page">
-            <h2 className="page-title">All Quiz Results</h2>
+            <h2 className="page-title">{t("allQuizResults")}</h2>
             <div className="topic-container">
                 {results.map((r, i) => (
                     <div className="topic-card" key={i}>
                         <h3>{r.topic}</h3>
-                        <p>Score: <span>{r.score}%</span></p>
+                        <p>{t("score")}: <span>{r.score}%</span></p>
                     </div>
                 ))}
             </div>
             <Link to={'/progress'}>
-                <button>Back</button>
+                <button>{t("back")}</button>
             </Link>
         </div>
     )
