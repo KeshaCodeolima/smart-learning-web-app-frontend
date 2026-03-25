546404a5988e768377003a2c457616169d72e4b1
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2026-03-25 14:57:28 +0530

    (localization): Add the localization in to the settings.jsx file.

--- src/Components/Header/Settings/settings.jsx
+++ src/Components/Header/Settings/settings.jsx
@@ -1,8 +1,10 @@
 import './settings.css';
 import LogoutIcon from '../../../Images/logouticon.png';
+import { useTranslation } from 'react-i18next';
 
 function Settings() {
     const user = JSON.parse(localStorage.getItem("currentuser")) || {};
+    const { t } = useTranslation();
     const logouthandle = () => {
         localStorage.clear();
         window.location.href = '/';
@@ -10,22 +12,22 @@ function Settings() {
     return (
         <>
             <div className="settingmain">
-                <h2>Setting page</h2>
+                <h2>{t("settings")}</h2>
                 <div className="settingmain2">
                     <div className="settingname">
-                        <label>Full Name</label>
-                        <span>{user.name || "Please Login"}</span>
+                        <label>{t("fullName")}</label>
+                        <span>{user.name || t("pleaseLogin")}</span>
                     </div>
                     <div className="settinguser">
-                        <label>UserName</label>
-                        <span>{user.username || 'Please Login'}</span>
+                        <label>{t("username")}</label>
+                        <span>{user.username || t("pleaseLogin")}</span>
                     </div>
                     <div className="settingemail">
-                        <label>Email</label>
-                        <span>{user.email || "Please Login"}</span>
+                        <label>{t("email")}</label>
+                        <span>{user.email || t("pleaseLogin")}</span>
                     </div>
                 </div>
-                <button className='settingbtn' onClick={logouthandle}>Logout
+                <button className='settingbtn' onClick={logouthandle}>{t("logout")}
                     <img src={LogoutIcon} alt="LogoutIcon" className='logouticonbtn' />
                 </button>
             </div>
